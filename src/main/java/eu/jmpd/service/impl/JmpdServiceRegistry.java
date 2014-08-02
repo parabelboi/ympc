dc8d7e53a657b83dfbde2fe4179cdca311326027
Christian Krafft <parabelboi@gmail.com>
2014-08-02 21:39:08 +0200

    checkin registry which uses annotaded model classes
    Signed-off-by: Christian Krafft <parabelboi@gmail.com>

--- src/main/java/eu/jmpd/service/impl/JmpdServiceRegistry.java
+++ src/main/java/eu/jmpd/service/impl/JmpdServiceRegistry.java
@@ -1,20 +1,30 @@
 package eu.jmpd.service.impl;
 
+import org.apache.olingo.odata2.annotation.processor.api.AnnotationServiceFactory;
 import org.apache.olingo.odata2.api.ODataService;
 import org.apache.olingo.odata2.api.ODataServiceFactory;
-import org.apache.olingo.odata2.api.edm.provider.EdmProvider;
+import org.apache.olingo.odata2.api.exception.ODataApplicationException;
 import org.apache.olingo.odata2.api.exception.ODataException;
 import org.apache.olingo.odata2.api.processor.ODataContext;
-import org.apache.olingo.odata2.api.processor.ODataSingleProcessor;
 
 public class JmpdServiceRegistry extends ODataServiceFactory {
+    private static class AnnotationInstances {
+        final static String MODEL_PACKAGE = "service.model";
+        final static ODataService ANNOTATION_ODATA_SERVICE;
+
+        static {
+          try {
+            ANNOTATION_ODATA_SERVICE = AnnotationServiceFactory.createAnnotationService(MODEL_PACKAGE);            
+          } catch (ODataApplicationException ex) {
+            throw new RuntimeException("Exception during sample data generation.", ex);
+          } catch (ODataException ex) {
+            throw new RuntimeException("Exception during data source initialization generation.", ex);
+          }
+        }
+      }
     
     @Override
     public ODataService createService(ODataContext ctx) throws ODataException {
-        
-        EdmProvider edmProvider = new JmpdEdmProvider();
-        ODataSingleProcessor singleProcessor = new JmpdODataSingleProcessor();
-        
-        return createODataSingleProcessorService(edmProvider, singleProcessor);
+        return AnnotationInstances.ANNOTATION_ODATA_SERVICE;
     }
 }
