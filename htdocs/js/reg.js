ba2aac41fe4785da5279cfa30abf19a62ce91de2
Christian Krafft <parabelboi@gmail.com>
2014-08-09 14:34:33 +0200

    remove merge conflict

--- htdocs/js/reg.js
+++ htdocs/js/reg.js
@@ -1,11 +1,7 @@
-/* jmpd
+/* ympc
    (c) 2013-2014 Andrew Karpow <andy@ndyk.de>
    (c) 2014 Christian Krafft <parabelboi@gmail.com>
-<<<<<<< HEAD
    This project's homepage is: http://www.ympc.eu
-=======
-   This project's homepage is: http://www.jmpd.eu
->>>>>>> extract web resources
    
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -44,11 +40,7 @@ function getMetaData() {
 			OData.defaultMetadata.push(metadata);
 			$('.top-right').notify({
 				message : {
-<<<<<<< HEAD
 					text : "connected to ympc"
-=======
-					text : "connected to ympd"
->>>>>>> extract web resources
 				},
 				fadeOut : {
 					enabled : true,
@@ -126,58 +118,6 @@ function removeEntry(setName, id, tableName) {
 		});
 	} catch (exception) {
 		alert('<p>Error' + exception);
-<<<<<<< HEAD
-	}
-}
-
-$("#addservicebtn").on('click', function(e) {
-	try {
-		OData.request({
-			requestUri : "/registry/Services",
-			method : "POST",
-			data : {
-				Id : $("#serviceId").val(),
-				Name : $("#serviceName").val(),
-				Url : $("#serviceUrl").val(),
-			}
-		}, function(insertedItem) {
-			$( "<tr id="+insertedItem.Id+">"
-				+ "<td>" + insertedItem.Id + "</td>"
-				+ "<td>" + insertedItem.Name + "</td>"
-				+ "<td>" + insertedItem.Url + "</td>"
-				+ "<td><button class=\"btn btn-default\" onclick=\"removeEntry(\'Services\',\'"+insertedItem.Id+"\',\'#services\');\">-</button></td>"
-			+ "</tr>").appendTo($("#services > tbody"));			
-		}, function(err) {
-			console.log("Error occurred");
-		});
-	} catch (exception) {
-		alert('<p>Error' + exception);
-	}
-});
-
-$("#addproviderbtn").on('click', function(e) {
-	try {
-		OData.request({
-			requestUri : "/registry/Providers",
-			method : "POST",
-			data : {
-				Id : $("#providerId").val(),
-				Name : $("#providerName").val(),
-			}
-		}, function(insertedItem) {
-			$( "<tr id="+insertedItem.Id+">"
-				+ "<td>" + insertedItem.Id + "</td>"
-				+ "<td>" + insertedItem.Name + "</td>"
-				+ "<td><button class=\"btn btn-default\" onclick=\"removeEntry(\'Providers\',\'"+insertedItem.Id+"\',\'#providers\');\">-</button></td>"
-			+ "</tr>").appendTo($("#providers > tbody"));			
-		}, function(err) {
-			console.log("Error occurred");
-		});
-	} catch (exception) {
-		alert('<p>Error' + exception);
-	}
-});
-=======
 	}
 }
 
@@ -230,7 +170,6 @@ $("#addproviderbtn").on('click', function(e) {
 });
 
 
->>>>>>> extract web resources
 
 $('#btnnotify').on('click', function(e) {
 	if ($.cookie("notification") === "true") {
